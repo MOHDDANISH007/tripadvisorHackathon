fe82a7d3578c1d53fdcc32cd10986fb6cceed424
Mohammad Danish <m.danish00707@gmail.com>
2025-04-17 23:34:50 +0530

    Start The Project

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return(
+    <div>
+      <h1 className="text-3xl font-bold underline p-4">hello world </h1>
+    </div>
+  )
+}
